4be78d75034d810ad1871736c2370b134ac9e76d
jeanfraga95 <112832054+jeanfraga95@users.noreply.github.com>
2026-04-13 20:53:34 -0300

    Create channels.py

---
+++ channels.py
@@ -0,0 +1,29 @@
+# channels.py — Definição dos canais skyMais
+
+CHANNELS = {
+    "aee": {
+        "name": "A&E",
+        "page_url": "https://www.skymais.com.br/home/pdp/live/CH0100000000110",
+        "channel_id": "CH0100000000110",
+    },
+    "amc": {
+        "name": "AMC",
+        "page_url": "https://www.skymais.com.br/player/live/CH0100000000082",
+        "channel_id": "CH0100000000082",
+    },
+    "amc-series": {
+        "name": "AMC Series",
+        "page_url": "https://www.skymais.com.br/player/live/CH0100000000308",
+        "channel_id": "CH0100000000308",
+    },
+    "animal-planet": {
+        "name": "Animal Planet",
+        "page_url": "https://www.skymais.com.br/player/live/CH0100000000116",
+        "channel_id": "CH0100000000116",
+    },
+    "axn": {
+        "name": "AXN",
+        "page_url": "https://www.skymais.com.br/player/live/CH0100000000086",
+        "channel_id": "CH0100000000086",
+    },
+}
